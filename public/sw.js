ae154a5f9c08da92b8aa9b54f55a4b5d2fa9de54
Maxim Siebert <maxim.siebert@gmail.com>
2026-02-12 00:50:28 -0500

    try again

--- public/sw.js
+++ public/sw.js
@@ -1,2 +1,24 @@
-// Minimal service worker for PWA installability
-self.addEventListener('fetch', () => {})
+const CACHE = 'syft-v1'
+const PRECACHE = ['/explore.html']
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE).then((cache) => cache.addAll(PRECACHE))
+  )
+  self.skipWaiting()
+})
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(self.clients.claim())
+})
+
+self.addEventListener('fetch', (event) => {
+  // Always go to network, fall back to cache for navigation
+  event.respondWith(
+    fetch(event.request).catch(() =>
+      event.request.mode === 'navigate'
+        ? caches.match('/explore.html')
+        : Response.error()
+    )
+  )
+})
